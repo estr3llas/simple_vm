8d89f64f418cac856e81a041d48c3364e27977f4
estr3llas <nao24nao69nao420@gmail.com>
2024-07-05 17:39:45 -0300

    added math demonstration

--- src/main.cpp
+++ src/main.cpp
@@ -46,14 +46,45 @@ int main (int argc, char** argv) {
         HALT                        // 13
     };
 
+    std::vector<int> math = {
+    // MATH DEMONSTRATION
+    // PUSH 1, 2
+        ICONST, 1,                  // 0
+        ICONST, 2,                  // 2
+    // 1 + 2 = 3
+        IADD,                       // 3
+    // PUSH 1
+        ICONST, 1,                  // 4
+    // 3 - 1 = 2
+        ISUB,                       // 6
+    // PUSH 2
+        ICONST, 2,                  // 7
+    // 2 * 2 = 4
+        IMUL,                       // 9
+    // PUSH 4
+        ICONST, 4,                  // 10
+    // 4 * 4 = 16
+        IMUL,                       // 12
+    // PUSH 2
+        ICONST, 2,                  // 13
+    // 16 / 2 = 8
+        IDIV,                       // 15
+    // PRINT(8)
+        PRINT,                      // 16
+        HALT                        // 17
+    };
 
     VM vm_loop(bytecode, MAIN_ADDR, 0);
     //vm_loop.SetTrace(vm_loop, VM_TRUE);
     vm_loop.cpu(vm_loop);
 
-    
+    /*
     VM vm_exception(exception_divide_by_zero, MAIN_ADDR, 0);
     vm_exception.SetTrace(vm_exception, VM_TRUE);
     vm_exception.cpu(vm_exception);
+    */
     
+    VM vm_math(math, 0, 0);
+    vm_math.SetTrace(vm_math, VM_TRUE);
+    vm_math.cpu(vm_math);
 }
