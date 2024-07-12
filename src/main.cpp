e4e2f8ff26acbf2c09dcb501c766a6059e6ed5e1
estr3llas <nao24nao69nao420@gmail.com>
2024-07-12 18:38:15 -0300

    cleaned main

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,7 @@
 
 int main (int argc, char** argv) {
 
-    if(argc < 2) {
-        printf(USAGE, VM_VERSION);
-    };
+    if(argc < 2) printf(USAGE, VM_VERSION);
 
     VM vm (Examples::test, Examples::TEST_ENTRYPOINT, 0);
     vm.SetTrace(VM_TRUE);
